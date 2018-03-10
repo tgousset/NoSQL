689f7775dcadeb6c7a3670182a21d4ce5bed728c
tgousset <tifenn.gousset@gmail.com>
2018-03-10 15:12:25 +0100

    Remplissage de la base de données. Affichage dans la console des données de la base.

--- src/main/java/Model/App.java
+++ src/main/java/Model/App.java
@@ -15,23 +15,60 @@ public class App {
     morphia.map(Person.class).map(Address.class);
     Datastore ds = morphia.createDatastore(mongo, "nosql");
 
-    Person p = new Person();
-    p.setName("Tintin");
-
+    // Address
     Address address = new Address();
     address.setStreet("123 Some street");
     address.setCity("Some city");
     address.setPostCode("123 456");
     address.setCountry("Some country");
-    //set address
+
+    Address address2 = new Address();
+    address2.setStreet("47 street view");
+    address2.setCity("New York");
+    address2.setPostCode("668 845");
+    address2.setCountry("USA");
+
+    //Person
+    Person p = new Person();
+    p.setName("Tintin");
+
+    Person p2 = new Person();
+    p2.setName("Mary");
+
+    //Article
+    Article a = new Article("Poster",5);
+    Article a2 = new Article("Computer",4);
+
+    //Add
     p.addAddress(address);
+    p2.addAddress(address2);
+    a.addBuyer(p);
+    a.addBuyer(p2);
+
     // Save the POJO
-
     ds.save(address);
+    ds.save(address2);
     ds.save(p);
-    for (Person e : ds.find(Person.class))
-      System.err.println(e);
+    ds.save(p2);
+    ds.save(a);
+    ds.save(a2);
 
+    System.out.println("Liste des personnes :");
+    for (Person e : ds.find(Person.class)) {
+      System.out.println("Name : " + e.getName());
+      for (Address addr : e.getAddressList()) {
+        System.out.println("Addresse  : " + addr.getStreet() + " " + addr.getPostCode() + " " +  addr.getCity() + " " + addr.getCountry());
+      }
+    }
+
+    System.out.println("Liste des articles :");
+
+    for (Article article : ds.find(Article.class)){
+      System.out.println("Article : " + article.getName() + " ; Stars : " + article.getStars());
+      for (Person person : article.getBuyers()){
+        System.out.println("Person : " + person.getName());
+      }
+    }
   }
 
 
