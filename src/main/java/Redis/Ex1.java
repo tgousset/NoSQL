4b826c69724af1440b2ccdc772ad72c44f2a30e9
tgousset <tifenn.gousset@gmail.com>
2018-03-13 20:35:33 +0100

    TP5 Ex1

---
+++ src/main/java/Redis/Ex1.java
@@ -0,0 +1,17 @@
+package Redis;
+
+import redis.clients.jedis.Jedis;
+
+public class Ex1 {
+
+  public static void main( String[] args )
+  {
+    Jedis jedis = new Jedis("localhost");
+    jedis.set("foo", "bar");
+    String value = jedis.get("foo");
+
+    System.err.println(value);
+  }
+
+
+}
